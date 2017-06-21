dfd0c974d63156763b70a6da6e91ffba37bc6b07
Nico Sta. Ana <Nico Sta. Ana>
2017-06-21 12:36:52 -0400

    first upload, missing files

---
+++ ClozeCard.js
@@ -0,0 +1,13 @@
+var ClozeCard = function(fullText, cloze){
+	this.fullText = fullText;
+	this.cloze = cloze;
+	this.partial = partial;
+
+}
+
+ClozeCard.prototype.logError = function (){
+
+	
+}
+
+module.exports = ClozeCard;
