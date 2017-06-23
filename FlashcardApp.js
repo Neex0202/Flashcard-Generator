b8461a34cbde96a15eed29720ffd5eadd7547f25
Nico Sta. Ana <Nico Sta. Ana>
2017-06-22 23:59:39 -0400

    added functional Cloze Card functions

--- FlashcardApp.js
+++ FlashcardApp.js
@@ -69,7 +69,10 @@ var flashCardBasicFour = function() {
 
         console.log(cardFour.back);
 
-        // flashCardBasicFive();
+        
+        // Calling chain of Cloze Card functions after executing flashCardBasicFour function
+
+        flashCardClozeOne();
     });
 
 };
@@ -91,30 +94,125 @@ var flashCardBasicFour = function() {
 
 
 // Start the Chain of inquirer Functions starting from cardOne
-flashCardBasicOne();
+// flashCardBasicOne();
 
 
 // Create instances from ClozeCard Constructor && set parameters
 var clozeOne = new ClozeCard("George Washington was the First President of the United States", "George Washington");
+var clozeTwo = new ClozeCard("The Federalists and Democratic Republicans were the first two political parties of the the United States", "Federalists");
+var clozeThree = new ClozeCard("During America's colonization, the Iroquois League was compromised by the Mohawk, Onondaga, Oneida, Cayuga, Seneca, and Tuscarora", "Seneca");
+var clozeFour = new ClozeCard("George Washington was the First President of the United States", "George Washington");
+
+
+
 
 // Testing ClozeCard Construct -WORKING!
-console.log(clozeOne.fullText);
-console.log(clozeOne.cloze);
-console.log(clozeOne.partial);
+// console.log(clozeOne.fullText);
+// console.log(clozeOne.cloze);
+// console.log(clozeOne.partial);
 
 
-// inquirer.prompt([
-// 	{	
-// 	name: "clozeOne",
-// 	message: 
-// 	}, {
+var flashCardClozeOne = function() {
 
-// 	},
+    inquirer.prompt([{
+        name: "clozeOne",
+        message: clozeOne.partial
+    }]).then(function(answers) {
+
+    		// Setting variable userAns to user input
+    		var userAns = answers.clozeOne;
+
+    		// Setting variable fullUserAnswer to clozed text + user input (replacing ellipse with input)
+    		var fullUserAnswer = clozeOne.partial.replace("...", userAns);
+
+        // Conditional to check if answer is correct
+        if(clozeOne.fullText == fullUserAnswer){
+        	// Displays if fullText matches User Input + this.partial
+        	console.log("Correct!");
+        	console.log(clozeOne.fullText);
+        }else{
+        	console.log("Incorrect!");
+        	console.log("------------------------------------------------------------------");
+        	console.log("The right answer:");
+        	console.log(clozeOne.fullText);
+        	console.log("------------------------------------------------------------------");
+        }
+
+        flashCardClozeTwo();
+        
+    });
+
+};
+
+var flashCardClozeTwo = function() {
+
+    inquirer.prompt([{
+    		type: "input",
+        name: "clozeTwo",
+        message: clozeTwo.partial
+    }]).then(function(answers) {
+
+    		// Setting variable userAns to user input
+    		var userAns = answers.clozeTwo;
+
+    		// Setting variable fullUserAnswer
+    		var fullUserAnswer = clozeTwo.partial.replace("...", userAns);
+
+        // Conditional to check if answer is correct
+        if(clozeTwo.fullText == fullUserAnswer){
+
+        	console.log("Correct!")
+        	console.log(clozeTwo.fullText);
+
+        }else{
+        	console.log("Incorrect!");
+        	console.log("------------------------------------------------------------------");
+        	console.log("The right answer:");
+        	console.log(clozeTwo.fullText);
+        	console.log("------------------------------------------------------------------");
+        }
+
+        flashCardClozeThree();
+        
+    });
+
+};
 
 
+var flashCardClozeThree = function() {
+
+    inquirer.prompt([{
+    		type: "input",
+        name: "clozeThree",
+        message: clozeThree.partial
+    }]).then(function(answers) {
+
+    		// Setting variable userAns to user input
+    		var userAns = answers.clozeThree;
+
+    		// Setting variable fullUserAnswer
+    		var fullUserAnswer = clozeThree.partial.replace("...", userAns);
+
+        // Conditional to check if answer is correct
+        if(clozeThree.fullText == fullUserAnswer){
+
+        	console.log("Correct!")
+        	console.log(clozeThree.fullText);
+
+        }else{
+        	console.log("Incorrect!");
+        	console.log("------------------------------------------------------------------");
+        	console.log("The right answer:");
+        	console.log(clozeThree.fullText);
+        	console.log("------------------------------------------------------------------");
+        }
+
+        // flashCardClozeTFour();
+        
+    });
+
+};
 
 
-// ]).then(function(answers){
-
-
-// });
+// Starting Chain of functions starting with Basic Flash cards
+flashCardBasicOne();
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Flashcard-Generator
+ # Flashcard-Generator
+
+ UNDER CONSTRUCTION!
+
+ Things to add
+ 	-process.argv[2] command to designate which set of flashcards to use
+ 	OR
+ 	-use inquirer.prompt for user to choose between card sets
